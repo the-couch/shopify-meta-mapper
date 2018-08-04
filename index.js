bcdbbe47c8103a3d18e5a1eacb5ddc842bc7cb3d
iamkevingreen <kevin@thecouch.nyc>
2018-08-03 22:24:55 -0400

    updates to the functionality add some initial error handling

--- index.js
+++ index.js
@@ -9,7 +9,64 @@ server.use(bodyParser.json({type: 'application/*'}))
 
 const landing = require('./landing.js')
 
+const Shopify = require('shopify-api-node')
+
 const { PORT } = process.env
+
+serverRouter.post('/api/update', (req, res) => {
+  const shopifyInfo = req.body
+
+  const shopifyAPI = new Shopify({
+    shopName: shopifyInfo.store_name,
+    apiKey: shopifyInfo.store_apikey,
+    password: shopifyInfo.store_password
+  })
+  shopifyAPI.product.list({ limit: 250, page: 0 })
+    .then(
+      (products) => {
+        console.log('hey', products)
+      }, () => {
+        console.log('bad err')
+        res.writeHead(200, {
+          'Content-Type': 'application/json'
+        })
+        res.write(JSON.stringify({
+          errors: [
+            'Issue accessing Shopify'
+          ]
+        }))
+        res.end()
+      }
+      // products => {
+      //   products.map(async ({id}) => {
+      //     const metafields = await shopifyAPI.metafield.list({
+      //       limit: 250,
+      //       metafield: { owner_resource: 'product', owner_id: id }
+      //     })
+      //     // console.log('what?', metafields)
+      //     const oldField = metafields.find(
+      //       ({ namespace, key }) => namespace === 'global' && key === 'testing'
+      //     )
+      //
+      //     if (!oldField) return
+      //
+      //     const newField = metafields.find(
+      //       ({ namespace, key }) => namespace === 'sf_product_land' && key === 'updated'
+      //     )
+      //
+      //     if (newField) {
+      //       // console.log('new field', newField)
+      //       // since the newfield exists let's update it
+      //       shopifyAPI.metafield.update(newField.id, {
+      //         value: oldField.value
+      //       }).then(res => console.log('response', res))
+      //     }
+      //
+      //     // console.log('there is an old field', oldField)
+      //   })
+      // }
+    )
+})
 
 serverRouter.get('*', (req, res) => {
   res.write(landing)
@@ -23,3 +80,5 @@ server.listen(PORT || 3001, () => {
 })
 
 // {{ product.metafields["global"]["testing"] }}
+
+// move to: product.metafields.sf_product_land.updated
--- landing.js
+++ landing.js
@@ -1,8 +1,12 @@
+
 module.exports = (
   `
     <div>
-      <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
+      <!-- <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet"> -->
       <style>
+        * {
+          box-sizing: border-box;
+        }
         body, html {
           font-family: 'Space Mono';
         }
@@ -32,9 +36,31 @@ module.exports = (
         }
         .s-i {
           font-family: 'Space Mono';
-          width: 32%;
           outline: none;
           padding: 1rem;
+        }
+        .s-s {
+          width: 32%;
+        }
+        .logger {
+          width: 100%;
+          height: 140px;
+          padding: 5px;
+          background-color: #000;
+        }
+        .logger span {
+          font-size: 0.7rem;
+          color: white;
+        }
+        form label span {
+          padding-bottom: 30px;
+        }
+        form button {
+          border: 0;
+          cursor: pointer;
+          padding: 8px 12px;
+          background-color: black;
+          color: white;
         }
       </style>
       <div class='container'>
@@ -42,33 +68,84 @@ module.exports = (
           Shopify Meta Field Mapper
         </h2>
         <div class='content'>
-          Simple solution for porting existing shopify meta field content to other fields.
+          Simple solution for porting existing shopify meta field content to other fields. You will need to create a private app inside of your shopify store in order to use this tool.
         </div>
         <div style='margin-top: 20px;'>
-          <form>
-            <div style='margin-bottom: 20px; display: flex; justify-content: space-between; padding-bottom: 20px; border-bottom: 1px solid #ccc;'>
-              <input class='s-i' name='store-name' placeholder='shopify-store-name' />
-              <input class='s-i' name='store-apikey' placeholder='Shopify API key' />
-              <input class='s-i' name='store-password' placeholder='Shopify Password' />
+          <form id='shopifyForm'>
+            <div style='margin-bottom: 40px; display: flex; justify-content: space-between; padding-bottom: 40px; border-bottom: 1px solid #ccc;'>
+              <input required class='s-i s-s' name='store_name' placeholder='shopify-store-name' />
+              <input required class='s-i s-s' name='store_apikey' placeholder='Shopify API key' />
+              <input required class='s-i s-s' name='store_password' placeholder='Shopify Password' />
             </div>
-            <div style='margin-bottom: 10px;'>
-              <label for='current'>
-                <span>Current Field</span>
-                <input class='ml05 s-i' name='current' placeholder='Original Field' />
+            <div style='margin-bottom: 10px; display: flex; justify-content: space-between;'>
+              <label for='current_namespace' style='width: 48%'>
+                <span>Original global namespace field</span><br />
+                <input required class=' s-i'  style='width: 100%;' name='current_namespace' placeholder='Original Global Namespace' />
+              </label>
+              <label for='current_key' class='ml05' style='width: 48%'>
+                <span>Original key field</span><br />
+                <input required class='s-i' style='width: 100%;' name='current_key' placeholder='Original Key Field' />
+              </label>
+            </div>
+            <div style='margin-bottom: 10px; display: flex; justify-content: space-between; padding-top:10px; border-top: 1px solid #ccc;'>
+              <label for='new_namespace' style='width: 48%'>
+                <span>New global namespace field</span><br />
+                <input required class=' s-i'  style='width: 100%;' name='current_namespace' placeholder='New Global Namespace' />
+              </label>
+              <label for='new_key' class='ml05' style='width: 48%'>
+                <span>New key field</span><br />
+                <input required class='s-i' style='width: 100%;' name='newkey' placeholder='New Key Field' />
               </label>
             </div>
             <div>
-              <label for='updated'>
-                <span>New Field</span>
-                <input class='ml05 s-i' name='updated' placeholder='New Field' />
-              </label>
+              <button type='submit'>Pew pew pew</button>
             </div>
           </form>
+        </div>
+        <div class='logger'>
+          <span>this is the logger...</span>
         </div>
         <div class='content'>
           <p>Created by <a href="https://thecouch.nyc">The Couch</a></p>
         </div>
       </div>
+      <script>
+        function toJSONString( form ) {
+          var obj = {};
+          var elements = form.querySelectorAll( "input, select, textarea" );
+          for( var i = 0; i < elements.length; ++i ) {
+            var element = elements[i];
+            var name = element.name;
+            var value = element.value;
+
+            if( name ) {
+              obj[ name ] = value;
+            }
+          }
+
+          return JSON.stringify( obj );
+        }
+
+        var form = document.getElementById('shopifyForm')
+
+        form.addEventListener('submit', (e) => {
+          e.preventDefault()
+          var jsonForm = toJSONString(form)
+          fetch('/api/update', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+            },
+            body: jsonForm
+          }).then((response) => {
+            console.log(response)
+            return response.json()
+          }).then((json) => {
+            console.log(json)
+          })
+        })
+      </script>
     </div>
   `
 )
